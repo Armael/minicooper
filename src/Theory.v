fc841428263d11d7eb9d6e5ebfa8410fa488b159
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2018-11-08 18:02:32 +0100

    Fix

--- src/Theory.v
+++ src/Theory.v
@@ -1,14 +1,14 @@
 Set Implicit Arguments.
 Require Import ZArith Psatz.
-Open Scope Z_scope.
 Require Import Znumtheory.
 Require Import Classical.
 Require Import FunInd Recdef.
 Require Import List.
 Import ListNotations.
-Open Scope list_scope.
-
 Require Import MiniCooper.MyTactics.
+Open Scope list_scope.
+Open Scope Z_scope.
+
 
 (* ------------------------------------------------------------------------- *)
 
